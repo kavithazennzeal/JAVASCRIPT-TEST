e9313edd0649a51d83783464b928245fe0e0b5b8
kavithashivani <kavithavenkatesh2440@gmail.com>
2022-01-27 14:32:01 +0530

    Test on 27-01-22

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+const kavi =[{palindrome:"madam",age:21},
+{palindrome:"teacher",age:22}]
+function fn()
+{
+    const kavi =arr.some((index,data)=>
+    {
+        return data=== palindrome
+    });
+    console.log(kavi);
+}
+fn();
